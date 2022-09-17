8fba0db6c6ddd1a4fc0f5299d0b7d2b7e782e6cb
Jean Campelo <jean93campelo@gmail.com>
2022-09-17 14:43:53 -0300

    Renderizando tamanhos

--- src/components/ViewProduct.js
+++ src/components/ViewProduct.js
@@ -11,6 +11,14 @@ export default function ViewProduct() {
           <h1> {product.name} </h1>
           <h2>Descrição</h2>
           <h3> {product.description} </h3>
+          <Sizes>
+            <h2> Tamanhos </h2>
+            <div>
+              {product.sizes.map((value) => (
+                <div className="details">{value}</div>
+              ))}
+            </div>
+          </Sizes>
         </Informations>
       </Product>
     </Container>
@@ -20,13 +28,12 @@ export default function ViewProduct() {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #fff;
+  background-color: #fffafa;
   display: flex;
   justify-content: center;
 `;
 
 const Product = styled.div`
-  background-color: #055555;
   display: flex;
   flex-direction: column;
 
@@ -37,7 +44,6 @@ const Product = styled.div`
 
 const Informations = styled.div`
   font-family: "Outfit", sans-serif;
-  background-color: #ffff00;
   margin-left: 16px;
 
   h1 {
@@ -53,5 +59,24 @@ const Informations = styled.div`
     font-weight: 400;
     margin-top: 2px;
     font-size: 14px;
+  }
+`;
+
+const Sizes = styled.div`
+  div {
+    display: flex;
+  }
+
+  .details {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #4b4b4c;
+    width: 24px;
+    height: 24px;
+    border: 1px solid #4b4b4c;
+    border-radius: 50px;
+    margin-right: 8px;
+    font-size: 12px;
   }
 `;
