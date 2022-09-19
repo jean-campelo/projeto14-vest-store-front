4d0ae5c2bf5c654938953d429930b4be63c6ba0c
Jean Campelo <jean93campelo@gmail.com>
2022-09-19 14:17:36 -0300

    deploy axios

--- src/assets/services/request.js
+++ src/assets/services/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://vest-store.herokuapp.com/';
 
 export function getHome(token) {
     const promise = axios({
